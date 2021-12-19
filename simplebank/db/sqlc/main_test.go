e164790a72aa68debc0ec699408dcc411f92051c
Yaolin_Li <we1586505611@163.com>
2021-12-19 23:09:01 +0800

    learn db lock

--- simplebank/db/sqlc/main_test.go
+++ simplebank/db/sqlc/main_test.go
@@ -2,25 +2,27 @@ package db
 
 import (
 	"database/sql"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
-	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 const(
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:123456@localhost:5432/simple_bank?sslmode=disable"
 )
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
